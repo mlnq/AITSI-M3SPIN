657c7f4444e2936c8539c0a23ac5197156b0bf73
Mateusz Siemieniuk <mati.s2806@interia.pl>
2022-06-01 20:52:59 +0200

    [QSP] First iteration of QSR

--- src/main/java/aitsi/m3spin/query/QueryResultProjector.java
+++ src/main/java/aitsi/m3spin/query/QueryResultProjector.java
@@ -1,6 +1,11 @@
 package aitsi.m3spin.query;
 
+import aitsi.m3spin.commons.interfaces.Statement;
+import aitsi.m3spin.commons.interfaces.TNode;
+import aitsi.m3spin.query.model.result.actual.AttrListResult;
+import aitsi.m3spin.query.model.result.actual.BooleanResult;
 import aitsi.m3spin.query.model.result.actual.QueryResult;
+import aitsi.m3spin.query.model.result.actual.TNodeSetResult;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
@@ -19,6 +24,38 @@ public class QueryResultProjector {
     * Elementy odpowiedzi są unikalne, tzn. bez powtórzeń. - ewentualnie do Evaluatora
     * */
     public String formatResult(List<QueryResult> rawResult) {
-        return null;
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        int index = 0;
+        for(QueryResult queryResult : rawResult)
+        {
+            if(queryResult.getClass() == BooleanResult.class) {
+                stringBuilder.append(((BooleanResult) queryResult).get());
+            }
+            else if (queryResult.getClass() == TNodeSetResult.class) {
+                int counter=0;
+                for(TNode currentNode : ((TNodeSetResult) queryResult).getResult())
+                {
+                    if(counter!=0)
+                        stringBuilder.append(" ");
+
+                    stringBuilder.append(((Statement) currentNode).getStmtLine());
+                    counter++;
+                }
+            }
+            else if (queryResult.getClass() == AttrListResult.class){
+
+            }
+            else {
+
+            }
+
+            if(index < rawResult.size()-1) {
+                stringBuilder.append(", ");
+            }
+            index++;
+        }
+        return stringBuilder.toString();
     }
 }
